fab7c9e96de26b0d2851515b32eee5026e295fb0
iamKhang <iamhoangkhang@icloud.com>
2024-05-31 21:20:41 +0700

    Khởi tạo Project

---
+++ src/test/java/com/lehoangkhang/SpringSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lehoangkhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
