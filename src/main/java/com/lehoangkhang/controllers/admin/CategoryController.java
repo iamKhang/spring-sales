52fee5930e6ec752117bffef778e63def6263ace
iamKhang <iamhoangkhang@icloud.com>
2024-06-01 10:28:01 +0700

    Xây dựng cấu trúc dự án, ghép giao diện admin

---
+++ src/main/java/com/lehoangkhang/controllers/admin/CategoryController.java
@@ -0,0 +1,20 @@
+package com.lehoangkhang.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/category")
+public class CategoryController {
+	
+	@GetMapping("")
+	public String index() {
+		return "admin/category/index";
+	}
+	
+	@GetMapping("/add")
+	public String add() {
+		return "admin/category/add";
+	}
+}
