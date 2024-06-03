a42a95bc0ebb6b841c489ed4956c1ab08125d475
iamKhang <iamhoangkhang@icloud.com>
2024-06-03 19:10:26 +0700

    Hiển thị thông tin người dùng đăng nhập và chức năng logout bằng thymeleaf-extras-springsecurity6

--- src/main/java/com/lehoangkhang/SecurityConfig.java
+++ src/main/java/com/lehoangkhang/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,7 +21,8 @@ public class SecurityConfig {
 				.authorizeRequests(auth -> auth.requestMatchers("/*").permitAll().requestMatchers("/admin/**")
 						.hasAuthority("Manager").anyRequest().authenticated())
 				.formLogin(login -> login.loginPage("/logon").loginProcessingUrl("/logon").usernameParameter("username")
-						.passwordParameter("password").defaultSuccessUrl("/admin", true));
+						.passwordParameter("password").defaultSuccessUrl("/admin", true))
+				.logout(logout -> logout.logoutUrl("/admin-logout").logoutSuccessUrl("/logon") );
 		return http.build();
 	}
 	
